4c94065464a64b91de86d1912c6e51da924e7e78
John Abele <jdabele@gmail.com>
2016-11-12 23:26:42 -0600

    Initial project code base

---
+++ SignalRGame/src/SignalRGame/Hubs/GameHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRGame.Hubs
+{
+    public class GameHub : Hub
+    {
+        public static List<string> ConnectedUsers;
+
+        public void Send(string originatorUser, string message)
+        {
+            Clients.All.messageReceived(originatorUser, message);
+        }
+
+        public void Connect(string newUser)
+        {
+            if (ConnectedUsers == null)
+                ConnectedUsers = new List<string>();
+
+            ConnectedUsers.Add(newUser);
+            Clients.Caller.getConnectedUsers(ConnectedUsers);
+            Clients.Others.newUserAdded(newUser);
+        }
+    }
+}
---
+++ SignalRGame/src/SignalRGame/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SignalRGame
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR(options =>
+            {
+                options.Hubs.EnableDetailedErrors = true;
+            });
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseStaticFiles();
+            app.UseWebSockets();
+            app.UseSignalR();
+            
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
